84bb1e1253952c7fe6cd77cdbae08966613bc1d6
Yohan Beschi <yohan.beschi@gmail.com>
2015-03-01 05:48:23 +0100

    Fixing test

--- test/loop/confidence/algorithms/AlgorithmsConfidenceTest.java
+++ test/loop/confidence/algorithms/AlgorithmsConfidenceTest.java
@@ -46,7 +46,7 @@ public class AlgorithmsConfidenceTest extends LoopTest {
 
   @Test
   public final void ransomNote() {
-    assertEquals("{f=2, g=1, d=2,  =14, e=5, b=1, c=1, a=1, n=4, o=9, l=1, N=1, m=3, .=1, k=1, h=2, i=5, w=2, u=3, t=5, s=6, r=5, Y=1, y=1}",
+    assertEquals("{ =14, a=1, b=1, c=1, d=2, e=5, f=2, g=1, h=2, i=5, k=1, l=1, m=3, N=1, n=4, .=1, o=9, r=5, s=6, t=5, u=3, w=2, y=1, Y=1}",
         Loop.run("test/loop/confidence/algorithms/ransom_note.loop").toString());
   }
 
